3c32e0916e0433b6b672a044237cbca00dfef7ac
Liakat Hossain <liakat89@gmail.com>
2023-07-08 14:41:32 +0200

    update directory

---
+++ ZeroCart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZeroCart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
